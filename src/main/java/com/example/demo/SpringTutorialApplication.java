bfa486634cd31d4ab10b99f97cec56005b4f05ee
BrendanCheong <brendancej82@gmail.com>
2021-08-04 16:18:51 +0800

    New Student Class

--- src/main/java/com/example/demo/SpringTutorialApplication.java
+++ src/main/java/com/example/demo/SpringTutorialApplication.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @SpringBootApplication
+/* RestController means that we allow this java file to have Rest API endpoints
+ * GetMapping means that this function is a GET API endpoint
+ * Remember to start the server after every change
+ */
+@RestController
 public class SpringTutorialApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringTutorialApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 
 }
---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,74 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    // Constructor for the student class default
+    public Student() {
+
+    }
+
+    //Constructor for student class everything
+    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    //Constructor for student class no id, because database already auto gives id
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+    //Getters and Setters below since attributes are private
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
